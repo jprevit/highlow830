6d93233aa468a162616a8085479bddb0d17b5282
John Previtera <johngprevitera@gmail.com>
2025-08-30 12:38:25 -0600

    basic functions up, working on styling

---
+++ app.js
@@ -0,0 +1,36 @@
+let maxNumber = 100
+let targetNumber = Math.round( Math.random() * maxNumber )
+
+function checkGuess(guess){
+    let outputElm = document.getElementById('output')
+
+    if (guess == targetNumber){
+        console.log(`Congratulations ${guess} was the number!`);
+        outputElm.innerText += `Congratulations ${guess} was the number!`
+
+    } else if (guess < targetNumber){
+        console.log(`${guess} is too small, try again`);
+        outputElm.innerText += `${guess} is too small, try again`
+        
+    } else if (guess > targetNumber) {
+        console.log(`${guess} is too big, think smaller!`);
+        outputElm.innerText += `${guess} is too big, think smaller!`
+    } else {
+        console.log('Input error, try again');
+    }
+
+}
+
+function drawButtons(){
+    let userInputElm = document.getElementById('user-input')
+    
+    userInputElm.innerHTML = ''
+
+    for (let button = 1; button <= maxNumber; button += 1){
+        userInputElm.innerHTML += `<button onclick="checkGuess(${button})">${button}</button>`
+    }
+
+    // console.log(`Final ${userInputElm.innerHTML}`);  
+}
+
+drawButtons()
